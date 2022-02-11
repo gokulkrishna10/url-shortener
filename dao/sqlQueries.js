edeb09be163e0407b966e834f9edbaba77b5ffc6
Gokul <gokul@DACs-MacBook-Pro.local>
2022-02-11 10:51:32 +0530

    no change

--- dao/sqlQueries.js
+++ dao/sqlQueries.js
@@ -16,8 +16,6 @@ AND (RequestDate)<= DATE_FORMAT(?,"%Y-%m-%d %H:%i:%s")
 GROUP BY Date, APIVersion`;
 
 
-
-
 module.exports.GET_DAILY_USAGE_WITH_ENDPOINTS_AND_API_NAME_QUERY = `SELECT DATE(RequestDate) as Date, an.DisplayName as APIName , APIVersion, EndpointName, Count(*) as Count 
 FROM APIUsage au
 JOIN APIName an on au.APINameId = an.APINameId
@@ -33,9 +31,6 @@ where APIKey = ? and DisplayName = ?
 AND (RequestDate) >= DATE_FORMAT(?,"%Y-%m-%d %H:%i:%s") 
 AND (RequestDate)<= DATE_FORMAT(?,"%Y-%m-%d %H:%i:%s")  
 GROUP BY Date, APIVersion`;
-
-
-
 
 
 module.exports.GET_MONTHLY_USAGE_WITH_ENDPOINTS_QUERY = `SELECT DATE_FORMAT(RequestDate,'%M %Y') AS Month, an.DisplayName as APIName , APIVersion, EndpointName, Count(*) as Count 
@@ -55,8 +50,6 @@ AND (RequestDate)<= DATE_FORMAT(?,"%Y-%m-%d %H:%i:%s")
 GROUP BY Month, APIVersion`;
 
 
-
-
 module.exports.GET_MONTHLY_USAGE_WITH_ENDPOINTS_AND_API_NAME_QUERY = `SELECT DATE_FORMAT(RequestDate,'%M %Y') AS Month, an.DisplayName as APIName , APIVersion, EndpointName, Count(*) as Count 
 FROM APIUsage au
 JOIN APIName an on au.APINameId = an.APINameId
@@ -72,8 +65,6 @@ where APIKey = ? and DisplayName = ?
 AND (RequestDate) >= DATE_FORMAT(?,"%Y-%m-%d %H:%i:%s") 
 AND (RequestDate)<= DATE_FORMAT(?,"%Y-%m-%d %H:%i:%s")  
 GROUP BY Month, APIVersion`;
-
-
 
 
 module.exports.GET_YEARLY_USAGE_WITH_ENDPOINTS_QUERY = `SELECT YEAR(RequestDate) AS Year, an.DisplayName as APIName , APIVersion, EndpointName, Count(*) as Count 
@@ -93,8 +84,6 @@ AND (RequestDate)<= DATE_FORMAT(?,"%Y-%m-%d %H:%i:%s")
 GROUP BY Year, APIVersion`;
 
 
-
-
 module.exports.GET_YEARLY_USAGE_WITH_ENDPOINTS_AND_API_NAME_QUERY = `SELECT YEAR(RequestDate) AS Year, an.DisplayName as APIName , APIVersion, EndpointName, Count(*) as Count 
 FROM APIUsage au
 JOIN APIName an on au.APINameId = an.APINameId
@@ -110,8 +99,6 @@ where APIKey = ? and DisplayName = ?
 AND (RequestDate) >= DATE_FORMAT(?,"%Y-%m-%d %H:%i:%s") 
 AND (RequestDate)<= DATE_FORMAT(?,"%Y-%m-%d %H:%i:%s") 
 GROUP BY Year, APIVersion`;
-
-
 
 
 //----------------------Error Queries-------------------------
@@ -136,8 +123,6 @@ AND au.APIErrorId IS NOT NULL
 LIMIT ?`;
 
 
-
-
 module.exports.GET_DAILY_ERROR_COUNT = `SELECT DATE(RequestDate) as Date, an.DisplayName as APIName , APIVersion, EndpointName, Count(*) as Count
 FROM APIUsage au
 JOIN APIName an on au.APINameId = an.APINameId
@@ -157,8 +142,6 @@ AND (RequestDate) >= DATE_FORMAT(?,"%Y-%m-%d %H:%i:%s")
 AND (RequestDate)<= DATE_FORMAT(?,"%Y-%m-%d %H:%i:%s") 
 AND au.APIErrorId IS NOT NULL
 GROUP BY Date, APIVersion`;
-
-
 
 
 module.exports.GET_MONTLY_ERROR_COUNT = `SELECT DATE_FORMAT(RequestDate,'%M %Y') AS Month, an.DisplayName as APIName , APIVersion, EndpointName, Count(*) as Count
@@ -182,7 +165,6 @@ AND au.APIErrorId IS NOT NULL
 GROUP BY Month, APIVersion`;
 
 
-
 module.exports.GET_YEARLY_ERROR_COUNT = `SELECT YEAR(RequestDate) AS Year, an.DisplayName as APIName , APIVersion, EndpointName, Count(*) as Count
 FROM APIUsage au
 JOIN APIName an on au.APINameId = an.APINameId
@@ -204,8 +186,6 @@ AND au.APIErrorId IS NOT NULL
 GROUP BY Year, APIVersion`;
 
 
-
-
 module.exports.GET_DAILY_ADMIN_USAGE_WITH_ENDPOINTS_QUERY = `SELECT DATE(RequestDate) as Date, an.DisplayName as APIName , APIVersion, EndpointName, Count(*) as Count 
 FROM APIUsage au
 JOIN APIName an on au.APINameId = an.APINameId
@@ -221,8 +201,6 @@ where an.DisplayName = ?
 AND (RequestDate) >= DATE_FORMAT(?,"%Y-%m-%d %H:%i:%s") 
 AND (RequestDate)<= DATE_FORMAT(?,"%Y-%m-%d %H:%i:%s") 
 GROUP BY Date, APIVersion`;
-
-
 
 
 module.exports.GET_MONTHLY_ADMIN_USAGE_WITH_ENDPOINTS_QUERY = `SELECT DATE_FORMAT(RequestDate,'%M %Y') AS Month, an.DisplayName as APIName , APIVersion, EndpointName, Count(*) as Count 
@@ -242,8 +220,6 @@ AND (RequestDate)<= DATE_FORMAT(?,"%Y-%m-%d %H:%i:%s")
 GROUP BY Month, APIVersion`;
 
 
-
-
 module.exports.GET_YEARLY_ADMIN_USAGE_WITH_ENDPOINTS_QUERY = `SELECT YEAR(RequestDate) AS Year, an.DisplayName as APIName , APIVersion, EndpointName, Count(*) as Count 
 FROM APIUsage au
 JOIN APIName an on au.APINameId = an.APINameId
@@ -259,7 +235,6 @@ where an.DisplayName = ?
 AND (RequestDate) >= DATE_FORMAT(?,"%Y-%m-%d %H:%i:%s") 
 AND (RequestDate)<= DATE_FORMAT(?,"%Y-%m-%d %H:%i:%s") 
 GROUP BY Year, APIVersion`;
-
 
 
 module.exports.GET_DAILY_ADMIN_ERROR_COUNT = `SELECT DATE(RequestDate) as Date, an.DisplayName as APIName , APIVersion, EndpointName, Count(*) as Count
