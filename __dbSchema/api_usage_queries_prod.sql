3d596f659a987f4068220cd86b40a2d59d06d6a0
Sudheer Kumar <cvsudheer@gmail.com>
2022-05-04 18:35:05 +0530

    script

--- __dbSchema/api_usage_queries_prod.sql
+++ __dbSchema/api_usage_queries_prod.sql
@@ -311,6 +311,8 @@ VALUES (4,15,5,1,0,0,0.6);
 INSERT INTO APICustomerPricing (APINameId, APIRouteId, APICustomerId, APIPricingTierId, DiscountAmountPerCall, DiscountPercentPerCall, SellingPricePerCall)
 VALUES (1,29,5,1,0,0,1.0);
 
+
+
 ###--------------------------------------------------------------------------------------------------
 #=========================================== Invoice Endpoint=============================================
 
@@ -321,20 +323,20 @@ Select * from APICustomerPricing;
 Set @startDate = DATE_FORMAT('2022-03-01 00:00:00','%Y-%m-%d %H:%i:%s');
 Set @endDate = DATE_FORMAT('2022-03-31 23:59:59','%Y-%m-%d %H:%i:%s');
 Select APIRouteId, SellingPricePerCall, StartDate, EndDate
-FROM APICustomerPricing acp 
-WHERE(
-	(EndDate > @startdate  AND EndDate < @endDate AND StartDate < @endDate )  #1.1
-	OR (StartDate < @endDate  AND EndDate >= @endDate)  #1.2
-	OR ((StartDate < @endDate) AND EndDate IS NULL)  #2.1
-	OR ((StartDate > @startdate  AND StartDate < @endDate) AND (EndDate > @startdate AND EndDate < @endDate)) #3.1
-	OR (StartDate = @startdate  AND EndDate = @endDate)
-)
-AND APIKey = 'PERSE-TEST-CLIENT-APIKEY';
-
+		FROM APICustomerPricing acp 
+        JOIN APICustomer ac on ac.APICustomerId = acp.APICustomerId
+		WHERE ac.APIKey = '67afbb34-2191-4e14-9b03-4b827dd74ff9'
+        AND (
+			(EndDate > @startdate  AND EndDate < @endDate AND StartDate < @endDate )  #1.1
+			OR (StartDate < @endDate  AND EndDate >= @endDate)  #1.2
+			OR ((StartDate < @endDate) AND EndDate IS NULL)  #2.1
+			OR ((StartDate > @startdate  AND StartDate < @endDate) AND (EndDate > @startdate AND EndDate < @endDate)) #3.1
+			OR (StartDate = @startdate  AND EndDate = @endDate)
+		);
 
 ##-- Get the Invoice fees for a given period
-Set @startDate = DATE_FORMAT('2022-03-01 00:00:00','%Y-%m-%d %H:%i:%s');
-Set @endDate = DATE_FORMAT('2022-03-31 23:59:59','%Y-%m-%d %H:%i:%s');
+Set @startDate = DATE_FORMAT('2022-04-01 00:00:00','%Y-%m-%d %H:%i:%s');
+Set @endDate = DATE_FORMAT('2022-04-30 23:59:59','%Y-%m-%d %H:%i:%s');
 
 SELECT au.APIRouteId, acp.SellingPricePerCall, an.DisplayName as APIName , 
 	au.APIVersion, 
@@ -348,7 +350,7 @@ SELECT au.APIRouteId, acp.SellingPricePerCall, an.DisplayName as APIName ,
 		Select APIRouteId, SellingPricePerCall, StartDate, EndDate
 		FROM APICustomerPricing acp 
         JOIN APICustomer ac on ac.APICustomerId = acp.APICustomerId
-		WHERE ac.APIKey = '4e628695-23ed-4759-9529-b4fc19e0b0b7'
+		WHERE ac.APIKey = '67afbb34-2191-4e14-9b03-4b827dd74ff9'
         AND (
 			(EndDate > @startdate  AND EndDate < @endDate AND StartDate < @endDate )  #1.1
 			OR (StartDate < @endDate  AND EndDate >= @endDate)  #1.2
@@ -357,13 +359,13 @@ SELECT au.APIRouteId, acp.SellingPricePerCall, an.DisplayName as APIName ,
 			OR (StartDate = @startdate  AND EndDate = @endDate)
 		)
     ) acp on acp.APIRouteId = au.APIRouteId
-    WHERE APIKey = '4e628695-23ed-4759-9529-b4fc19e0b0b7'
+    WHERE APIKey = '67afbb34-2191-4e14-9b03-4b827dd74ff9'
 	AND RequestDate >= @startDate
 	AND RequestDate <= @endDate
 	AND an.DisplayName != 'Half Hourly Meter History API'
 	GROUP BY APIName, au.APIVersion, au.EndpointName
     Order By SellingPricePerCall desc;
-    
+
 
 #### Fixx the Routeids in APIUsage table
 
@@ -375,8 +377,6 @@ Where EndpointName = @epName
 AND APIVersion = @apiVersion
 AND APIRouteId != @routeId;
 
-
-
 Update APIUsage
 Set APIRouteId = @routeId
 Where EndpointName = @epName
@@ -384,8 +384,15 @@ AND APIVersion = @apiVersion
 AND APIRouteId != @routeId;
 
 
+## ===================================Load Testing Analysis======================================
+#1. Find the used service end points order by Usage
+Select an.Name, au.EndpointName, Count(*) as Count from APIUsage au
+Join APIName an on au.APINameId = an.APINameId
+#Join APIRoute ar on ar.APIRouteId = au.APIRouteid
+Group by au.APINameId, au.EndpointName;
 
-SET SQL_SAFE_UPDATES = 0;
 
-SET SQL_SAFE_UPDATES = 1;
 
+select *  from APIUsage
+Order By APIUsageId desc
+LIMIT 30;
